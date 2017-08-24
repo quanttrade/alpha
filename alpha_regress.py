1428aaa0315e7e804bfe61232368efafb4f66a9c
liyizheng <lyizheng@outlook.com>
2017-08-24 23:35:10 +0800

    alpha_model_backtest

--- alpha_regress.py
+++ alpha_regress.py
@@ -1,6 +1,7 @@
 from barra_factor import *
 from neutralize import *
 from scipy import stats
+from cvxpy import *
 
 
 def alpha_delete(alpha_corr, alpha_IR, corr_limit, ir_limit, period):
@@ -129,6 +130,156 @@ def factor_information_coefficient_statistics(ic):
         ic_summary_table["IC Kurtosis"] = stats.kurtosis(ic)
         ic_summary_table["Ann. IR"] = (
             ic.mean() / ic.std()) * np.sqrt(252)
+
+
+def stock_weight(bench_weight, risk_factor, volume_t, returns_t, alpha_factor, alpha_returns, upper_bound, TC, w_old):
+    """
+    return the potofolio weight which make the returns best
+
+    Parameters
+    bench_weight: Seriees
+    the cap_weight of the benchmark
+
+    risk_factor: DataFrame
+    barra_factor including risk factors and industry factors
+
+    volume_t :Series
+    the volume if today
+
+    returns_t: Series
+    the retuens of today
+
+    alpha_factor: DataFrame
+    the loading the alpha_factor
+
+    factor_returns: DataFrame
+
+    upper_bound: float
+    the upper bound of stock weight
+
+    TC: float
+    Turnover fee
+
+    w_old: array
+    the stock weight last day
+    """
+    # first caculate the bench loading
+    bench_risk_loading = risk_factor.ix[bench_weight.index].values.T.dot(bench_weight.values)
+
+    #restrict the stock tradeable
+    risk_factor = risk_factor[volume_t > 0]
+    risk_factor = risk_factor[returns_t.abs()] < 0.1
+    alpha_factor = alpha_factor[risk_factor.index]
+
+    #defne the weight vector to solve
+    N = risk_factor.shape[0]
+    w = Variable(N, 1)
+
+    #caculate the potofolio loading
+    potofolio_risk_loading = risk_factor.values.T * w
+    alpha_loading = alpha_factor.values.T * w
+    untrade_weight = 0.0
+    #the weight of last day
+    if isinstance(w_old, float):
+        w_last = 0.0
+
+    else:
+        w_last = pd.Series(np.repeat(0.0, N), index=risk_factor.index)
+        stock_t = list(set(w_last.index) & set(w_old.index))
+        w_last.ix[stock_t] = w_old.ix[stock_t]
+        untrade_stock = list(set(w_old.index) - set(w_last.index))
+        for stock in untrade_stock:
+            untrade_weight += w_old[stock]
+
+    #define the object to solve
+    ret = alpha_loading * alpha_returns - TC * abs(w - w_last)
+
+    constraints = [w >= 0, w <= upper_bound, sum_entries(w) == 1, abs(potofolio_risk_loading - bench_risk_loading) < 0.01]
+    prob = Problem(Maximize(ret), constraints)
+    Maximize_value = prob.solve()
+    weight = array(w.value)
+    weight = pd.Series([weight[i][0] for i in range(len(weight))], index=risk_factor.index) * (1 - untrade_weight)
+
+    if untrade_stock:
+        for stock in untrade_stock:
+            weight[stock] = w_old[stock]
+    return weight
+
+
+
+
+def alpha_model_backtest(barra_factor, alpha_facotr, alpha_returns, benchmark_component, price_data, upper_bound, TC):
+
+
+    volume = price_data['volume']
+    returns = price_data['adjclose'].pct_change(period).shift(-period)
+    cap = price_data['close'] * price_data['total_shares']
+    barra_factor.index = barra_factor.index.rename(['date', 'asset'])
+
+    tradedate = barra_factor.index.get_level_values('date')
+    tradedate = list(set(tradedate))
+    tradedate.sort()
+    tradedate = pd.Series(tradedate)
+    reblance_day = tradedate.ix[tradedate.index % 2 == 0]
+
+    weight_dict = dict()
+    now_weight = pd.Series()
+
+    Lambda = np.power(0.5, 1.0 / 60.0)
+    decay_weight = np.array([Lambda ** (249 - i) for i in range(i)])
+
+    for date in tradedate:
+        if date not in reblance_day:
+            weight_dict[date] = now_weight
+        else:
+            cap_t = cap.ix[date]
+            returns_t = returns.ix[date]
+            volume_t = volume.ix[date]
+
+            risk_factor = barra_factor.ix[date]
+            benchmark_component_date = benchmark_component.ix[date]
+            bench_weight = cap_t.ix[risk_factor.index].ix[benchmark_component_date].dropna()
+            bench_weight = bench_weight / bench_weight.sum()
+
+            if now_weight.empty:
+                w_old = 0.0
+
+            else:
+                w_old = now_weight.copy()
+
+
+            alpha_returns_date = np.average(alpha_returns.ix[:date][-250:], weights=decay_weight, axis=1)
+
+            reblance_weight = stock_weight(bench_weight=bench_weight, risk_factor=risk_factor,
+                                           volume_t=volume_t, returns_t=returns,
+                                           alpha_factor=alpha_facotr.ix[date], alpha_returns=alpha_returns_date
+                                           upper_bound=upper_bound, TC=TC, w_old=w_old)
+
+            now_weight = reblance_weight
+
+            weight_dict[date] = now_weight
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     gtja_path = 'E:\gtja_alpha'
